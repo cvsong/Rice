695ffe952db3804b9653659ed82bd4ba30789230
cvsong <491175590@qq.com>
2018-03-09 15:15:27 +0800

    新增工具module

---
+++ common_library/src/main/java/com/cvsong/study/common_library/base/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.cvsong.study.common_library.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * BaseActivity-->负责业务逻辑无关的常用功能封装
+ *
+ * Created by chenweisong on 2018/3/9.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
